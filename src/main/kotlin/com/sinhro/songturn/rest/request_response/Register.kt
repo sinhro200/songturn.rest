2d5aac93edeecd7782316296dd0be8d2906fc2f0
sinhro <sinhro-200@mail.ru>
2021-01-27 17:35:39 +0300

    changed register req data

--- src/main/kotlin/com/sinhro/songturn/rest/request_response/Register.kt
+++ src/main/kotlin/com/sinhro/songturn/rest/request_response/Register.kt
@@ -1,29 +1,14 @@
 package com.sinhro.songturn.rest.request_response
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.sinhro.songturn.rest.model.RegisterUserInfo
 import com.sinhro.songturn.rest.validation.Contains
 import com.sinhro.songturn.rest.validation.MinMaxLength
 import com.sinhro.songturn.rest.validation.NotContains
 import com.sinhro.songturn.rest.validation.NotNull
 
 public class RegisterReqData(
-        @field:[MinMaxLength(5,50) NotNull NotContains("@.")]
-        val login: String,
-
-        @field:[MinMaxLength(5,50) NotNull]
-        val password: String,
-
-        @field:[MinMaxLength(5,50) NotNull]
-        val nickname: String,
-
-        @field:MinMaxLength(2,50)
-        val firstName: String,
-
-        @field:MinMaxLength(2,50)
-        val lastName: String,
-
-        @field:[MinMaxLength(1,90) Contains("@.")]
-        val email: String
+        val userInfo: RegisterUserInfo = RegisterUserInfo()
 )
 
 public class RegisterRespBody(
